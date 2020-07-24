2f071ba7c0285834a3e25f8308af2df8a719f8dd
flcamillo <fabio.camillo@gmail.com>
2020-07-24 10:53:47 -0300

    adicionado icones nos botões e funcionalidade para gerar csv

--- js/relconsolidado.js
+++ js/relconsolidado.js
@@ -13,7 +13,7 @@ function logoff() {
 }
 
 // função para realizar a pesquisa dos eventos
-function pesquisarEventos(event) {
+function pesquisarEventos(event, csv) {
     // previne a ação padrão do botão (envio do formulario, submit)
     event.preventDefault();
     // cria um novo formulario
@@ -27,7 +27,11 @@ function pesquisarEventos(event) {
     fetch("http://localhost:8080/evento/alarme/entre", requisicao)
         .then(response => {
             if (response.status == 200) {
-                response.json().then(dados => gerarRelatorio(dados));
+                if (csv == false) {
+                    response.json().then(dados => gerarRelatorio(dados));
+                } else {
+                    response.json().then(dados => gerarCSV(dados));
+                }
             } else {
                 document.getElementById("relatorio").innerHTML = `<p>Não foi possível realizar a consulta.<p>`;
             }
@@ -50,4 +54,22 @@ function gerarRelatorio(dados) {
     });
     relatorioTexto += `</tbody></table>`;
     relatorio.innerHTML = relatorioTexto;
+}
+
+// função para criar o arquivo CSV
+function gerarCSV(dados) {
+    let relatorio = document.getElementById("relatorio");
+    if (dados == null || dados.length == 0) {
+        relatorio.innerHTML = `<p>Nenhum registro encontrado.<p>`;
+        return;
+    }
+    let csv = "";
+    dados.forEach(e => {
+        csv += `${e[0]};${e[1]}\n`;
+    });
+    let hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = 'eventos_consolidado.csv';
+    hiddenElement.click(); 
 }
--- js/relevento.js
+++ js/relevento.js
@@ -13,11 +13,9 @@ function logoff() {
 }
 
 // função para realizar a pesquisa dos eventos
-function pesquisarEventos(event) {
+function pesquisarEventos(event, csv) {
     // previne a ação padrão do botão (envio do formulario, submit)
     event.preventDefault();
-    // cria um novo formulario
-    data = new FormData()
     // cria a requisição para enviar ao servidor
     let requisicao = {
         method: "POST",
@@ -27,7 +25,11 @@ function pesquisarEventos(event) {
     fetch("http://localhost:8080/evento/equipamento/entre", requisicao)
         .then(response => {
             if (response.status == 200) {
-                response.json().then(dados => gerarRelatorio(dados));
+                if (csv == false) { 
+                    response.json().then(dados => gerarRelatorio(dados));
+                } else {
+                    response.json().then(dados => gerarCSV(dados));
+                }
             } else {
                 document.getElementById("relatorio").innerHTML = `<p>Não foi possível realizar a consulta.<p>`;
             }
@@ -50,4 +52,22 @@ function gerarRelatorio(dados) {
     });
     relatorioTexto += `</tbody></table>`;
     relatorio.innerHTML = relatorioTexto;
+}
+
+// função para criar o arquivo CSV
+function gerarCSV(dados) {
+    let relatorio = document.getElementById("relatorio");
+    if (dados == null || dados.length == 0) {
+        relatorio.innerHTML = `<p>Nenhum registro encontrado.<p>`;
+        return;
+    }
+    let csv = "";
+    dados.forEach(e => {
+        csv += `${e.data};${e.alarme.nome};${e.equipamento.nomeHost}\n`;
+    });
+    let hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = 'eventos_equipamento.csv';
+    hiddenElement.click(); 
 }
